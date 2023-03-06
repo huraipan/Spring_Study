aa6d30e7f088b98969e165ba94c1999560ffc259
huraipan <chki247@naver.com>
2023-03-06 17:44:52 +0900

    JSR-250

---
+++ JSR250/src/main/java/kr/co/softcampus/beans/TestBean1.java
@@ -0,0 +1,17 @@
+package kr.co.softcampus.beans;
+
+public class TestBean1 {
+	
+	public TestBean1() {
+		System.out.println("TestBean1");
+	}
+	
+	public void init1() {
+		System.out.println("TestBean1's init");
+	}
+	
+	public void destroy1() {
+		System.out.println("TestBean1's destroy");
+	}
+
+}
---
+++ JSR250/src/main/java/kr/co/softcampus/beans/TestBean2.java
@@ -0,0 +1,26 @@
+package kr.co.softcampus.beans;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class TestBean2 {
+	
+	public TestBean2() {
+		System.out.println("TestBean2");
+	}
+	
+	
+	// 생성자 호출 이후 자동으로 호출
+	@PostConstruct
+	public void init2() {
+		System.out.println("TestBean2's init");
+	}
+	
+	
+	// 객체가 소멸되기 전에 자동으로 호출
+	@PreDestroy
+	public void destroy2() {
+		System.out.println("TestBean2's destroy");
+	}
+
+}
