2b902d00c8f67e1c68f8bc21b0325e6440619df2
huraipan <chki247@naver.com>
2023-03-22 13:39:12 +0900

    Message

---
+++ part2/MessageJava/src/main/java/kr/co/softcampus/config/ServletAppContext.java
@@ -0,0 +1,48 @@
+package kr.co.softcampus.config;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("kr.co.softcampus.controller")
+public class ServletAppContext implements WebMvcConfigurer{
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+	
+	
+	
+	// Properties -> message
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource res = new ReloadableResourceBundleMessageSource();
+		
+		//res.setBasename("/WEB-INF/properties/data1");
+		res.setBasenames("/WEB-INF/properties/data1", "/WEB-INF/properties/data2");
+		
+		return res;
+	}
+	
+	
+
+}
